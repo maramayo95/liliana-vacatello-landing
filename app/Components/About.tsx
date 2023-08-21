5d94925ae5b40d99fe66ca288f2cb59b69a352ee
Matias Aramayo <matiasaramayo@hotmail.com>
2023-08-21 19:35:15 -0300

    Corrección en about

--- app/Components/About.tsx
+++ app/Components/About.tsx
@@ -3,14 +3,14 @@ import Image from 'next/image'
 
 const About = () => {
   return (
-    <section className=" flex flex-col mt-10 bg-green-dark xl:flex-row  xl:py-14 w-[100vw]  ">
-      <div className="flex flex-col xl:flex-row m-auto w-full sm:container 2xl:max-w-screen-xl">
+    <section className=" flex flex-col mt-10 bg-green-dark xl:flex-row   w-[100vw]  ">
+      <div className="flex flex-col xl:flex-row m-auto w-full sm:container 2xl:max-w-screen-xl py-4">
         <div className="mt-6 flex justify-center items-center">
-            <Image className="py-5 w-full h-auto px-10" src="/about.png" alt="Picture of the author"  width={500} height={500}
+            <Image className="py-5 w-full px-10 height-auto" src="/about.png" alt="Picture of the author"  width={300} height={300}
             />
         </div>
 
-        <div className="px-10 my-6 xl:w-1/2  flex flex-col justify-center ">
+        <div className="px-10  xl:w-1/2  flex flex-col justify-center ">
             <h3 className="text-white font-semibold text-5xl xl:text-7xl">Liliana</h3>
             <h4 className="text-white font-semibold text-6xl xl:text-8xl">Vacatello</h4>
             <p className="mt-10 text-2xl font-light text-white">
