c52f81db65d95105d1096b7a195bbbe7dcad34d7
Matias Aramayo <matiasaramayo@hotmail.com>
2023-08-31 22:27:11 -0300

    .

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -7,8 +7,8 @@ const Hero = () => {
     <section className="flex flex-col mt-4 text-green-intermediate font-bold px-10  md:flex-row lg:flex-row md:items-center   m-auto w-full sm:container 2xl:max-w-screen-xl">
     
         <div className="xl:w-1/2">
-          <h1 className="mt-10 text-5xl xl:text-6xl ">Terapias integrativas ¿Qué es el coaching?</h1>
-          <p className="mt-10 text-2xl  font-light">Las terapias integrativas y el coaching se complementan para promover el bienestar holístico y el crecimiento personal. Mientras que las terapias integrativas buscan armonizar cuerpo y mente con prácticas como la meditación y el yoga, el coaching se enfoca en el desarrollo de habilidades y el logro de metas.</p>
+          <h1 className=" xl:mt-10 text-2xl xl:text-6xl ">Terapias integrativas ¿Qué es el coaching?</h1>
+          <p className="mt-10 text-lg xl:text-2xl  font-light">Las terapias integrativas y el coaching se complementan para promover el bienestar holístico y el crecimiento personal. Mientras que las terapias integrativas buscan armonizar cuerpo y mente con prácticas como la meditación y el yoga, el coaching se enfoca en el desarrollo de habilidades y el logro de metas.</p>
             <div className="mt-10">
                         <Link className='text-xl text-font-navbar py-4 px-6 text-center text-slate-200 bg-brown-light rounded-full border-b-2 md:border-b-0   ' href="#projects">
                           Hace tu pedido
--- app/Components/Services.tsx
+++ app/Components/Services.tsx
@@ -7,15 +7,15 @@ const Services = () => {
         <div className="  flex flex-col m-auto w-full sm:container 2xl:max-w-screen-xl xl:flex xl:flex-row">
            <article className="flex flex-col justify-center items-center">
               <Image src="/servicio_un.png" width="300" height="300" alt="Servicio uno"/>
-              <p className="text-center text-2xl font-light text-green-dark px-4 xl:text-base">Meditación y mindfulness brindan calma y conciencia en el presente, favoreciendo una respuesta serena ante los desafíos de la vida y promoviendo el bienestar integral.</p>
+              <p className="text-center text-lg xl:text-2xl font-light text-green-dark px-4 ">Meditación y mindfulness brindan calma y conciencia en el presente, favoreciendo una respuesta serena ante los desafíos de la vida y promoviendo el bienestar integral.</p>
            </article>
            <article className="flex flex-col justify-center items-center">
               <Image src="/service_do.png" width="300" height="300" alt="Servicio uno"/>
-              <p className="text-center text-2xl font-light text-green-dark px-4 xl:text-base">Meditación y mindfulness brindan calma y conciencia en el presente, favoreciendo una respuesta serena ante los desafíos de la vida y promoviendo el bienestar integral.</p>
+              <p className="text-center text-lg xl:text-2xl font-light text-green-dark px-4 ">Meditación y mindfulness brindan calma y conciencia en el presente, favoreciendo una respuesta serena ante los desafíos de la vida y promoviendo el bienestar integral.</p>
            </article>
            <article className="flex flex-col justify-center items-center">
               <Image src="/service_tre.png" width="300" height="300" alt="Servicio uno"/>
-              <p className="text-center text-2xl font-light text-green-dark px-4 xl:text-base">Meditación y mindfulness brindan calma y conciencia en el presente, favoreciendo una respuesta serena ante los desafíos de la vida y promoviendo el bienestar integral.</p>
+              <p className="text-center text-lg xl:text-2xl font-light text-green-dark px-4 ">Meditación y mindfulness brindan calma y conciencia en el presente, favoreciendo una respuesta serena ante los desafíos de la vida y promoviendo el bienestar integral.</p>
            </article>
         </div>
     </section>
